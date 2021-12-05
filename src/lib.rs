e2042cc76cc0e1115101d89c152097f85d7e6993
Cody P Schafer <dev@codyps.com>
2021-12-05 13:57:50 -0500

    more cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -9,18 +9,6 @@ mod std_time;
 #[cfg(feature = "std")]
 pub use std_time::*;
 
-const fn micros_from_duration(duration: time::Duration) -> u64 {
-    let sub_micros = (duration.subsec_nanos() / 1000) as u64;
-    duration.as_secs() * 1_000_000 + sub_micros
-}
-
-fn duration_from_micros(micros: u64) -> time::Duration {
-    let secs = micros / 1_000_000;
-    let sub_micros = (micros % 1_000_000) as u32;
-    let sub_nsec = sub_micros * 1000;
-    time::Duration::new(secs, sub_nsec)
-}
-
 const fn seconds_to_micros(seconds: u64) -> u64 {
     seconds * 1_000_000
 }
@@ -32,61 +20,10 @@ const fn millis_to_micros(millis: u64) -> u64 {
 /// An instant in monotonic time as provided by a `CLOCK_MONOTONIC` like clock
 ///
 /// Internally, this uses microsecond (micros) sized values to track, giving 584942417355.07202148
-/// years until overflow. Internally, systemd uses the same formatting for it's time values.
+/// years until overflow. Internally, systemd uses the same layout for it's time values.
 #[derive(Debug, Eq, PartialEq, PartialOrd, Ord, Clone, Copy)]
 pub struct MonotonicTime {
     micros: u64,
-}
-
-impl MonotonicTime {
-    pub const ZERO: Self = Self { micros: 0 };
-
-    /// Create a timestamp from microseconds
-    pub const fn from_micros(micros: u64) -> Self {
-        MonotonicTime { micros }
-    }
-
-    /// Return the entire timestamp converted to microseconds
-    pub const fn as_micros(&self) -> u64 {
-        self.micros
-    }
-
-    pub const fn from_millis(millis: u64) -> Self {
-        MonotonicTime {
-            micros: millis_to_micros(millis),
-        }
-    }
-
-    pub const fn from_seconds(seconds: u32) -> Self {
-        MonotonicTime {
-            micros: seconds as u64 * 1_000_000,
-        }
-    }
-}
-
-impl Sub<MonotonicTime> for MonotonicTime {
-    type Output = Duration;
-
-    fn sub(self, rhs: MonotonicTime) -> Self::Output {
-        Duration::from_micros(self.micros - rhs.micros)
-    }
-}
-
-/// Treat a duration as if it instead was a timestamp.
-///
-/// Sometimes, due to the lack of a way to generate a `std::time::Instant` from a numeric
-/// value, other code may use a `Duration` to contain a timestamp. In that case, you should use
-/// this function to obtain a `MonotonicTime` for the timestamp within the `Duration`
-impl From<MonotonicTime> for time::Duration {
-    fn from(s: MonotonicTime) -> time::Duration {
-        duration_from_micros(s.as_micros())
-    }
-}
-
-impl From<time::Duration> for MonotonicTime {
-    fn from(s: time::Duration) -> Self {
-        Self::from_micros(micros_from_duration(s))
-    }
 }
 
 /// A timestamp from a `CLOCK_REALTIME` like clock
@@ -95,20 +32,67 @@ pub struct RealTime {
     micros: u64,
 }
 
-impl RealTime {
-    pub const ZERO: Self = Self { micros: 0 };
-}
+macro_rules! time_math_impl {
+    ($time_type:ty) => {
+        impl $time_type {
+            pub const ZERO: Self = Self { micros: 0 };
 
-impl Add<time::Duration> for RealTime {
-    type Output = RealTime;
+            /// Create a timestamp from microseconds
+            pub const fn from_micros(micros: u64) -> Self {
+                Self { micros }
+            }
 
-    fn add(self, other: time::Duration) -> RealTime {
-        let mo: u64 = other.as_micros().try_into().unwrap();
-        RealTime {
-            micros: self.micros + mo,
+            /// Return the entire timestamp converted to microseconds
+            pub const fn as_micros(&self) -> u64 {
+                self.micros
+            }
+
+            pub const fn from_millis(millis: u64) -> Self {
+                Self {
+                    micros: millis_to_micros(millis),
+                }
+            }
+
+            pub const fn from_seconds(seconds: u32) -> Self {
+                Self {
+                    micros: seconds_to_micros(seconds as u64),
+                }
+            }
         }
-    }
+
+        impl Sub for $time_type {
+            type Output = Duration;
+
+            fn sub(self, rhs: Self) -> Self::Output {
+                Duration::from_micros(self.micros - rhs.micros)
+            }
+        }
+
+        impl Add<time::Duration> for $time_type {
+            type Output = Self;
+
+            fn add(self, other: time::Duration) -> Self {
+                let mo: u64 = other.as_micros().try_into().unwrap();
+                Self {
+                    micros: self.micros + mo,
+                }
+            }
+        }
+
+        impl Add<Duration> for $time_type {
+            type Output = Self;
+
+            fn add(self, other: Duration) -> Self {
+                Self {
+                    micros: self.micros + other.micros,
+                }
+            }
+        }
+    };
 }
+
+time_math_impl! {MonotonicTime}
+time_math_impl! {RealTime}
 
 #[derive(Debug, Eq, PartialEq, PartialOrd, Ord, Clone, Copy)]
 pub struct Duration {
@@ -130,5 +114,57 @@ impl Duration {
         Duration {
             micros: millis_to_micros(millis),
         }
+    }
+}
+
+impl Add for Duration {
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            micros: self.micros + rhs.micros,
+        }
+    }
+}
+
+impl Sub for Duration {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self {
+            micros: self.micros - rhs.micros,
+        }
+    }
+}
+
+// TODO: make this `const` once duration_consts_2 is stable
+fn duration_from_micros(micros: u64) -> time::Duration {
+    let secs = micros / 1_000_000;
+    let sub_micros = (micros % 1_000_000) as u32;
+    let sub_nsec = sub_micros * 1000;
+    time::Duration::new(secs, sub_nsec)
+}
+
+const fn micros_from_duration(duration: time::Duration) -> u64 {
+    let sub_micros = (duration.subsec_nanos() / 1000) as u64;
+    duration.as_secs() * 1_000_000 + sub_micros
+}
+
+impl MonotonicTime {
+    /// Treat a `MonotonicTime` as if it were a `core::time::Duration`
+    ///
+    /// Sometimes, due to the lack of a way to generate a `std::time::Instant` from a numeric
+    /// value, other code may use a `Duration` to contain a timestamp. This creates such a
+    /// duration.
+    pub fn to_core_duration(self) -> core::time::Duration {
+        duration_from_micros(self.micros)
+    }
+
+    /// Treat a duration as if it instead was a timestamp.
+    ///
+    /// Sometimes, due to the lack of a way to generate a `std::time::Instant` from a numeric
+    /// value, other code may use a `Duration` to contain a timestamp.  In that case, you should
+    /// use this function to obtain a `MonotonicTime` for the timestamp within the `Duration`
+    pub fn from_core_duration(ctd: core::time::Duration) -> Self {
+        Self::from_micros(micros_from_duration(ctd))
     }
 }
--- tests/t1.rs
+++ tests/t1.rs
@@ -1,23 +1,34 @@
-extern crate microtime;
-
-use std::convert::From;
-
+#[cfg(feature = "std")]
 #[test]
 fn realtime_systemtime_round_trip() {
     let us = 10_000_001;
-    let m_rt = microtime::RealTime::from_micros(us);
-    let s_rt = std::time::SystemTime::from(m_rt);
-    let m_rt2 = microtime::RealTime::from(s_rt);
+    let m_rt: microtime::RealTime =
+        microtime::RealTime::ZERO + microtime::Duration::from_micros(us);
+    let s_rt: std::time::SystemTime = m_rt.into();
+    let m_rt2: microtime::RealTime = s_rt.into();
 
-    assert_eq!(us, m_rt2.as_micros());
+    assert_eq!(us, (m_rt2 - microtime::RealTime::ZERO).as_micros());
 }
 
 #[test]
 fn monotonic_duration_round_trip() {
     let us = 10_000_001;
-    let m_t = microtime::MonotonicTime::from_micros(us);
-    let s_t = std::time::Duration::from(m_t);
-    let m_t2 = microtime::MonotonicTime::from(s_t);
+    let m_t: microtime::MonotonicTime =
+        microtime::MonotonicTime::ZERO + microtime::Duration::from_micros(us);
+    let s_t: core::time::Duration = m_t.to_core_duration();
+    let m_t2 = microtime::MonotonicTime::from_core_duration(s_t);
 
     assert_eq!(us, m_t2.as_micros());
+}
+
+const fn dur_1sec_10micro() -> microtime::Duration {
+    microtime::Duration::from_seconds(1) + microtime::Duration::from_micros(10)
+}
+
+#[test]
+fn const_check() {
+    assert_eq!(
+        dur_1sec_10micro(),
+        microtime::Duration::from_seconds(1) + microtime::Duration::from_micros(10)
+    );
 }
